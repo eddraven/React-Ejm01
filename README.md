0bb9b9d79a7090141a440c3776d2ec0d7eb3fca9
eddraven <eduardohr91@hotmail.com>
2022-07-18 18:12:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-2cbvur
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-2cbvur)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Ejemplo 01 en React</h1>
+      <p>Mi primera clase en react</p>
+    </div>
+  );
+}
