41b4662807c3664eba81fc389ee68f02b1dca587
eddraven <eduardohr91@hotmail.com>
2022-07-18 18:38:26 -0500

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
     <div>
       <h1>Ejemplo 01 en React</h1>
       <p>Mi primera clase en react</p>
-      <img src="./img/usuarios.png" alt="img-usuarios.png" />
     </div>
   );
 }
